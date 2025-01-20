d30618aa60b3cf2392cdb8bb5ca1e35da106ae66
Dan Jacobs <danjacobs@gmail.com>
2025-01-20 21:01:49 +0000

    logs back

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        // LogViewer::auth(function ($request) {
-        //     return true;
-        // });
+        LogViewer::auth(function ($request) {
+            return true;
+        });
 
     }
 }
